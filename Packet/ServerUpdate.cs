bb2a3e0cf6bf023dc917d8467a31e1ac85a94f4c
LastExceed <chris20194@googlemail.com>
2018-01-20 17:39:43 +0100

    update mission types

--- Packet/ServerUpdate.cs
+++ Packet/ServerUpdate.cs
@@ -299,14 +299,14 @@ namespace Resources.Packet {
             public int unknownB;
             public int unknownC;
             public int id;
-            public int unknownD;
+            public int type;
             public EntityType monsterID;
             public int level;
             public byte unknownE;
             public MissionState state; //0=ready 1=progressing 2=finished
             public short padding;
-            public float unknownF;
-            public float unknownG;
+            public float currentHP;
+            public float maxHP;
             public int chunkX;
             public int chunkY;
 
@@ -318,14 +318,14 @@ namespace Resources.Packet {
                 unknownB = reader.ReadInt32();
                 unknownC = reader.ReadInt32();
                 id = reader.ReadInt32();
-                unknownD = reader.ReadInt32();
+                type = reader.ReadInt32();
                 monsterID = (EntityType)reader.ReadInt32();
                 level = reader.ReadInt32();
                 unknownE = reader.ReadByte();
                 state = (MissionState)reader.ReadByte();
                 padding = reader.ReadInt16();
-                unknownF = reader.ReadSingle();
-                unknownG = reader.ReadSingle();
+                currentHP = reader.ReadSingle();
+                maxHP = reader.ReadSingle();
                 chunkX = reader.ReadInt32();
                 chunkY = reader.ReadInt32();
             }
@@ -337,14 +337,14 @@ namespace Resources.Packet {
                 writer.Write(unknownB);
                 writer.Write(unknownC);
                 writer.Write(id);
-                writer.Write(unknownD);
+                writer.Write(type);
                 writer.Write((int)monsterID);
                 writer.Write(level);
                 writer.Write(unknownE);
                 writer.Write((byte)state);
                 writer.Write(padding);
-                writer.Write(unknownF);
-                writer.Write(unknownG);
+                writer.Write(currentHP);
+                writer.Write(maxHP);
                 writer.Write(chunkX);
                 writer.Write(chunkY);
             }
