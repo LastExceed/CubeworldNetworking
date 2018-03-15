a3500c0db6367e958004bc82ed6b30bf021ac12f
LastExceed <chris20194@googlemail.com>
2018-03-15 22:43:33 +0100

    cleanup bit tools

--- Utilities/Tools.cs
+++ Utilities/Tools.cs
@@ -3,51 +3,42 @@ using System.Threading;
 
 namespace Resources {
     public static class Tools {
-        public static bool GetBit(this byte value, int bitNumber) {
-            return (value & (1 << bitNumber)) != 0;
+        public static bool GetBit(this byte number, int bitIndex) {
+            if (bitIndex > 7 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-7");
+            return (number & (1 << bitIndex)) != 0;
         }
-        public static bool GetBit(this int value, int bitNumber) {
-            return (value & (1 << bitNumber)) != 0;
+        public static bool GetBit(this int number, int bitIndex) {
+            if (bitIndex > 31 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-31");
+            return (number & (1 << bitIndex)) != 0;
         }
-        public static bool GetBit(this long value, int bitNumber) {
-            return (value & (1L << bitNumber)) != 0;
+        public static bool GetBit(this long number, int bitIndex) {
+            if (bitIndex > 63 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-64");
+            return (number & (1L << bitIndex)) != 0;
         }
 
-        /// <param name="bitnumber">0 based Position</param>
-        public static void SetBit(ref byte b, bool value, int bitnumber) {
-            if(bitnumber < 8 && bitnumber > -1) {
-                if(value) {
-                    b |= (byte)(0x01 << bitnumber);
-                } else {
-                    b &= (byte)~(0x01 << bitnumber);
-                }
-            } else {
-                throw new IndexOutOfRangeException("bitNumber must be between 0-7 for bytes");
-            }
+        /// <param name="bitIndex">0 based Position</param>
+        public static void SetBit(ref byte number, int bitIndex, bool bitValue) {
+            if (bitIndex > 7 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-7");
+            else if (bitValue)
+                number |= (byte)(1 << bitIndex);
+            else
+                number &= (byte)~(1 << bitIndex);
         }
-        /// <param name="bitNumber">0 based Position</param>
-        public static void SetBit(ref int b, bool value, int bitNumber) {
-            if(bitNumber < 32 && bitNumber > -1) {
-                if(value) {
-                    b |= (1 << bitNumber);
-                } else {
-                    b &= ~(1 << bitNumber);
-                }
-            } else {
-                throw new IndexOutOfRangeException("bitNumber must be between 0-31 for integers");
-            }
+        /// <param name="bitIndex">0 based Position</param>
+        public static void SetBit(ref int number, int bitIndex, bool bitValue) {
+            if (bitIndex > 31 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-31");
+            else if (bitValue)
+                number |= (1 << bitIndex);
+            else
+                number &= ~(1 << bitIndex);
         }
-        /// <param name="bitnumber">0 based Position</param>
-        public static void SetBit(ref long b, bool value, int bitnumber) {
-            if(bitnumber < 64 && bitnumber > -1) {
-                if(value) {
-                    b |= 1L << bitnumber;
-                } else {
-                    b &= ~(1L << bitnumber);
-                }
-            } else {
-                throw new IndexOutOfRangeException("bitNumber must be between 0-63 for bytes");
-            }
+        /// <param name="bitIndex">0 based Position</param>
+        public static void SetBit(ref long number, int bitIndex, bool bitValue) {
+            if (bitIndex > 63 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-63");
+            else if (bitValue)
+                number |= (1L << bitIndex);
+            else
+                number &= ~(1L << bitIndex);
         }
 
         /// <summary>
