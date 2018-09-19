3df062ec933a6d3d520b1f6a8ada767f37f07144
LastExceed <chris20194@googlemail.com>
2018-09-19 14:28:14 +0200

    add SetBit for short

--- Utilities/Bit.cs
+++ Utilities/Bit.cs
@@ -15,23 +15,34 @@ namespace Resources {
             if (bitIndex > 63 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-64");
             return (number & (1L << bitIndex)) != 0;
         }
-        
+
         public static void Set(ref byte number, int bitIndex, bool bitValue) {
-            if (bitIndex > 7 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-7");
+            if (bitIndex > 7 || bitIndex < 0)
+                throw new IndexOutOfRangeException("bitIndex must be in range 0-7");
             else if (bitValue)
                 number |= (byte)(1 << bitIndex);
             else
                 number &= (byte)~(1 << bitIndex);
         }
+        public static void Set(ref short number, int bitIndex, bool bitValue) {
+            if (bitIndex > 15 || bitIndex < 0)
+                throw new IndexOutOfRangeException("bitIndex must be in range 0-15");
+            else if (bitValue)
+                number |= (short)(1 << bitIndex);
+            else
+                number &= (short)~(1 << bitIndex);
+        }
         public static void Set(ref int number, int bitIndex, bool bitValue) {
-            if (bitIndex > 31 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-31");
+            if (bitIndex > 31 || bitIndex < 0)
+                throw new IndexOutOfRangeException("bitIndex must be in range 0-31");
             else if (bitValue)
                 number |= (1 << bitIndex);
             else
                 number &= ~(1 << bitIndex);
         }
         public static void Set(ref long number, int bitIndex, bool bitValue) {
-            if (bitIndex > 63 || bitIndex < 0) throw new IndexOutOfRangeException("bitIndex must be in range 0-63");
+            if (bitIndex > 63 || bitIndex < 0)
+                throw new IndexOutOfRangeException("bitIndex must be in range 0-63");
             else if (bitValue)
                 number |= (1L << bitIndex);
             else
