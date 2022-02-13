0e4cfaa8a66849cbf4e0945a7549d13bd312236c
LastExceed <chris20194@googlemail.com>
2022-02-13 08:43:27 +0100

    fix oversight in value class -> enum conversion

--- src/main/kotlin/packets/Packet.kt
+++ src/main/kotlin/packets/Packet.kt
@@ -4,28 +4,29 @@ import com.github.lastexceed.cubeworldnetworking.utils.*
 
 abstract class Packet(val packetId: PacketId) : CwSerializable
 
-@JvmInline
-value class PacketId(val value: Int) {
-	companion object {
-		val CreatureUpdate = PacketId(0)
-		val MultiEntityUpdate = PacketId(1)
-		val WaveClear = PacketId(2)
-		val AirTraffic = PacketId(3)
-		val ServerUpdate = PacketId(4)
-		val Time = PacketId(5)
-		val CreatureAction = PacketId(6)
-		val Hit = PacketId(7)
-		val StatusEffect = PacketId(8)
-		val Shot = PacketId(9)
-		val ChatMessage = PacketId(10)
-		val ChunkDiscovery = PacketId(11)
-		val SectorDiscovery = PacketId(12)
-		val Unknown13 = PacketId(13)
-		val Unknown14 = PacketId(14)
-		val MapSeed = PacketId(15)
-		val Join = PacketId(16)
-		val ProtocolVersion = PacketId(17)
-		val ServerFull = PacketId(18)
+enum class PacketId : CwSerializableEnumInt {
+	CreatureUpdate,
+	MultiEntityUpdate,
+	WaveClear,
+	AirTraffic,
+	ServerUpdate,
+	Time,
+	CreatureAction,
+	Hit,
+	StatusEffect,
+	Shot,
+	ChatMessage,
+	ChunkDiscovery,
+	SectorDiscovery,
+	Unknown13,
+	Unknown14,
+	MapSeed,
+	Join,
+	ProtocolVersion,
+	ServerFull;
+
+	companion object : CwEnumIntDeserializer<PacketId> {
+		override val values = values()
 	}
 }
 
