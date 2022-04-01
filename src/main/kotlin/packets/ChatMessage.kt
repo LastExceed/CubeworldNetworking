0650e7ecf2b1933ea00957ff562f71c3a61562fa
LastExceed <chris20194@googlemail.com>
2022-04-01 10:51:26 +0200

    split ChatMessage into `FromClient` and `FromServer` to comply with `CwDeserializer`

--- src/main/kotlin/packets/ChatMessage.kt
+++ src/main/kotlin/packets/ChatMessage.kt
@@ -2,23 +2,57 @@ package com.github.lastexceed.cubeworldnetworking.packets
 
 import com.github.lastexceed.cubeworldnetworking.utils.*
 
-data class ChatMessage(
-	val sender: CreatureId?,
-	val text: String
-) : Packet(PacketId.ChatMessage) {
+abstract class ChatMessage : Packet(PacketId.ChatMessage) {
+	abstract val text: String
+
 	override suspend fun writeTo(writer: Writer) {
-		if (sender != null) {
-			writer.writeLong(sender.value)
-		}
 		writer.writeInt(text.length)
 		writer.writeByteArray(text.toByteArray(Charsets.UTF_16LE))
 	}
 
-	companion object { //TODO: CwDeserializer<ChatMessage>
-		suspend fun readFrom(reader: Reader, readSender: Boolean) =
-			ChatMessage(
-				sender = if (readSender) CreatureId(reader.readLong()) else null,
-				text = String(reader.readByteArray(reader.readInt() * 2), Charsets.UTF_16LE)
-			)
+	data class FromClient(
+		override val text: String
+	) : ChatMessage() {
+		companion object : CwDeserializer<ChatMessage.FromClient> {
+			override suspend fun readFrom(reader: Reader) =
+				ChatMessage.FromClient(
+					text = readText(reader)
+				)
+		}
+	}
+
+	data class FromServer(
+		val source: CreatureId,
+		override val text: String
+	) : ChatMessage() {
+		override suspend fun writeTo(writer: Writer) {
+			writer.writeLong(source.value)
+			super.writeTo(writer)
+		}
+
+		companion object : CwDeserializer<ChatMessage.FromServer> {
+			override suspend fun readFrom(reader: Reader) =
+				ChatMessage.FromServer(
+					source = CreatureId(reader.readLong()),
+					text = readText(reader)
+				)
+		}
+	}
+
+	companion object {
+		private suspend fun readText(reader: Reader) =
+			String(reader.readByteArray(reader.readInt() * 2), Charsets.UTF_16LE)
+
+		/** The Cubeworld protocol is inconsistent here (blame Wollay).
+		 * When sent from client to server, this packet is missing one of its properties (namely [ChatMessage.FromServer.source]).
+		 * In order to stay compliant with [CwDeserializer], the packet was split into 2 subclasses:
+		 * @see [ChatMessage.FromClient.readFrom]
+		 * @see [ChatMessage.FromServer.readFrom] */
+		@Deprecated(
+			"See KDoc",
+			level = DeprecationLevel.ERROR,
+			replaceWith = ReplaceWith("ChatMessage.FromClient.readFrom(reader)")
+		)
+		fun readFrom(reader: Reader, readSender: Boolean): Nothing = error("unreachable")
 	}
 }
